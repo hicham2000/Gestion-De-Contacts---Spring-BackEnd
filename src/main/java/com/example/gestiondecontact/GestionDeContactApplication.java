fd24ae2be1ac4784085d67d028eff8c9f856dad3
hicham <oo906496@gmail.com>
2023-02-25 02:12:31 +0100

    adding model and controller

--- src/main/java/com/example/gestiondecontact/GestionDeContactApplication.java
+++ src/main/java/com/example/gestiondecontact/GestionDeContactApplication.java
@@ -6,6 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 
 @SpringBootApplication
 public class GestionDeContactApplication implements CommandLineRunner {
@@ -17,7 +21,35 @@ public class GestionDeContactApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        contactRepository.save(new Contact(null,"hicham","0600148141"));
+        DateFormat date =new SimpleDateFormat("dd/MM/yyyy");
+        contactRepository.save(new Contact(
+                null,
+                "taib",
+                "hicham",
+                date.parse("08/12/2000"),
+                "hicham@gmail.com",
+                "0600148141",
+                "photo1"
+
+        ));contactRepository.save(new Contact(
+                null,
+                "taib",
+                "omar",
+                date.parse("08/12/2000"),
+                "hicham@gmail.com",
+                "0600148141",
+                "photo1"
+
+        ));contactRepository.save(new Contact(
+                null,
+                "taib",
+                "abdelhak",
+                date.parse("08/12/2000"),
+                "hicham@gmail.com",
+                "0600148141",
+                "photo1"
+
+        ));
         contactRepository.findAll().forEach(e->{
             System.out.println(e);
         });
--- src/main/java/com/example/gestiondecontact/entities/Contact.java
+++ src/main/java/com/example/gestiondecontact/entities/Contact.java
@@ -1,16 +1,26 @@
 package com.example.gestiondecontact.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
 
 @Entity
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class Contact  {
+public class Contact implements Serializable {
+    @Id @GeneratedValue
+    private Long id;
+    private String nom;
+    private String prenom;
+    @Temporal(TemporalType.DATE)
+    private Date DateDeNaissance;
+    private String email;
+    private String tel;
+    private String photo;
 
 }
