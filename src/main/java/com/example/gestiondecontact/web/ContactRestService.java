7946f70a1db653463ad827524959673f7774fb82
hicham <oo906496@gmail.com>
2023-02-27 22:24:53 +0100

    adding pagination

--- src/main/java/com/example/gestiondecontact/web/ContactRestService.java
+++ src/main/java/com/example/gestiondecontact/web/ContactRestService.java
@@ -3,6 +3,10 @@ package com.example.gestiondecontact.web;
 import com.example.gestiondecontact.dao.ContactRepository;
 import com.example.gestiondecontact.entities.Contact;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -26,10 +30,7 @@ public class ContactRestService {
     public Optional<Contact> getContact(@PathVariable int id){
         return contactRepository.findById(id);
     }
-    @RequestMapping(value = "/chercherContact", method = RequestMethod.GET)
-    public List<Contact> getContact(@RequestParam(name = "mc",defaultValue = "") String mc){
-        return contactRepository.chercher("%"+mc+"%");
-    }
+
 @RequestMapping(value = "/contacts/{id}",method = RequestMethod.DELETE)
     public Boolean deleteContact(@PathVariable int id) {
         contactRepository.deleteById(id);
@@ -40,6 +41,15 @@ public class ContactRestService {
     public Contact saveContact(@PathVariable int id,@RequestBody Contact c){
         c.setId(id);
         return contactRepository.save(c);
+    }
+
+    @RequestMapping(value = "/chercherContact",method = RequestMethod.GET)
+    public Page<Contact> chercherContact(@RequestParam(value = "name" , defaultValue = "") String mc,
+                                         @RequestParam(value = "page", defaultValue = "0") int page,
+                                         @RequestParam(value = "size", defaultValue = "5") int size){
+
+        return contactRepository.chercher("%"+mc+"%",PageRequest.of(page,size));
+
     }
 
 }
